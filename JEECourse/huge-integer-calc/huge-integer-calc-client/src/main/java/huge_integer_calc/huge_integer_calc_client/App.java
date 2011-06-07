55e9b39e6cf014e7697bfa59b29ae1f81ad68971
unknown <michel.ruizf@gmail.com>
2011-06-06 21:41:26 -0300

    New Project Structure

---
+++ JEECourse/huge-integer-calc/huge-integer-calc-client/src/main/java/huge_integer_calc/huge_integer_calc_client/App.java
@@ -0,0 +1,13 @@
+package huge_integer_calc.huge_integer_calc_client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
